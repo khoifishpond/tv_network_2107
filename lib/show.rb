c3165955cee53484affedba0b409a82cff3ff4dd
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-21 08:25:11 -0700

    Create show class

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,26 @@
+class Show
+  attr_reader :name, :creator, :characters
 
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    all_salary = 0
+
+    @characters.each { |character| all_salary += character.salary }
+
+    all_salary
+  end
+
+  def highest_paid_actor
+    character_played_by = @characters.max_by { |character| character.salary }
+    character_played_by.actor
+  end
+
+  def actors
+    @characters.map { |character| character.actor }
+  end
+end
